076b027ea4253faaa8894d0f678e836ddfb00ac2
PARK-Yunjae <dbswoql0712@gmail.com>
2024-01-27 23:12:17 +0900

    update
    터치 퍼즐

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -1,9 +1,13 @@
+// 모바일 기기 감지 드래그 이벤트 재시도
+const isMobile = /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent)
+
 const start = document.querySelector(".startBtn");
+const body = document.body;
 const main = document.querySelector("main");
 let time = document.querySelector(".time");
 const bar = document.querySelector(".bar");
 const puzzle = document.querySelector(".puzzle");
-const size = 512;
+let size = 512;
 
 let level = 3;
 let interval = null;
@@ -18,10 +22,25 @@ const dragged = {
 };
 
 start.addEventListener("click", () => {
+    resizing();
     start.style.top = "-100px";
     initPuzzle();
     startPuzzle();
 })
+
+// 기본 가로세로 크기인 512 보다 작으면 줄이기
+function resizing() {
+    console.log(body.clientWidth);
+    console.log(main.style.width);
+    if (body.clientWidth <= 512) {
+        main.style.width = `${body.clientWidth}px`;
+        main.style.height = `${body.clientWidth + 40}px`;
+        puzzle.style.width = `${body.clientWidth}px`;
+        puzzle.style.height = `${body.clientWidth}px`;
+        size = body.clientWidth;
+        console.log(size);
+    }
+}
 
 // 퍼즐 초기화
 function initPuzzle() {
@@ -95,7 +114,7 @@ function shuffle(arr) {
 
 // 드래그 이벤트
 puzzle.addEventListener("dragstart", e => {
-    if (!isReady) return;
+    if (!isReady || isMobile) return;
     const obj = e.target;
 
     dragged.el = obj;
@@ -103,15 +122,38 @@ puzzle.addEventListener("dragstart", e => {
     dragged.index = [...obj.parentNode.children].indexOf(obj);
 })
 
+puzzle.addEventListener('touchstart', e => {
+    if (!isReady || !isMobile) return;
+    const obj = e.target;
+
+    dragged.el = obj;
+    dragged.class = obj.className;
+    dragged.index = [...obj.parentNode.children].indexOf(obj);
+});
+
 puzzle.addEventListener("dragover", e => {
-    if (!isReady) return;
+    if (!isReady || isMobile) return;
 
     e.preventDefault();
 })
 
 puzzle.addEventListener("drop", e => {
-    if (!isReady) return;
+    if (!isReady || isMobile) return;
 
+    changePuzzle(e);
+
+    checkIndex();
+})
+
+puzzle.addEventListener("touchend", e => {
+    if (!isReady || !isMobile) return;
+
+    changePuzzle(e);
+
+    checkIndex();
+})
+
+function changePuzzle(e) {
     const obj = e.target;
     if (obj.className !== dragged.class) {
         let originPlace;
@@ -127,9 +169,9 @@ puzzle.addEventListener("drop", e => {
         dragged.index > droppedIdx ? obj.before(dragged.el) : obj.after(dragged.el);
         isLast ? originPlace.after(obj) : originPlace.before(obj);
     }
+}
 
-    checkIndex();
-})
+
 
 // 퍼즐 같은 위치인지 체크 - 한줄 리턴 생략 가능하지만 너무 길어짐
 function checkIndex() {
